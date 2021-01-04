225aefc645db92dc151af84aa2af20aae0d24fb7
Catalina Boznea <catalina.boznea@tradeshift.com>
2021-01-04 16:41:45 +0100

    Rename appleSauce to totalAmount

--- public/bilPrinting.js
+++ public/bilPrinting.js
@@ -73,7 +73,7 @@ function totalVolumeCredits() {
 	return volumeCredits;
 }
 
-function appleSauce() {
+function totalAmount() {
 	let totalAmount = 0;
 	for (let perf of invoice.performances) {
 		totalAmount += amountFor(perf);
@@ -87,9 +87,7 @@ function statement(invoice, plays) {
 		// print line for this order
 		result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
 	}
-	let totalAmount = appleSauce();
-
-	result += `Amount owed is ${usd(totalAmount)}\n`;
+	result += `Amount owed is ${usd(totalAmount())}\n`;
 	result += `You earned ${totalVolumeCredits()} credits\n`;
 	return result;
 }
