896e5a04ffab528c1c393132a12f639a7665298d
Catalina Boznea <catalina.boznea@tradeshift.com>
2021-01-04 18:04:29 +0100

    Add getVolumentCredit function

--- public/createStatementData.js
+++ public/createStatementData.js
@@ -11,7 +11,7 @@ export default function createStatementData(invoice, plays) {
 		const result = Object.assign({}, aPerformance);
 		result.play = calculator.play;
 		result.amount = calculator.amount;
-		result.volumeCredits = volumeCreditsFor(result);
+		result.volumeCredits = calculator.volumeCredits;
 		return result;
 	}
 	function playFor(aPerformance) {
@@ -60,6 +60,13 @@ class PerformanceCalculator {
 			default:
 				throw new Error(`unknown type: ${this.play.type}`);
 		}
+		return result;
+	}
+
+	getVolumeCredits() {
+		let result = 0;
+		result += Math.max(this.performance.audience - 30, 0);
+		if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
 		return result;
 	}
 }
