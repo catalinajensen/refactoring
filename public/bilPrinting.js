6278e02a50f0e5a9779562f8d9de7907f972da9b
Catalina Boznea <catalina.boznea@tradeshift.com>
2021-01-04 17:15:17 +0100

    Move volumeCreditsFor

--- public/bilPrinting.js
+++ public/bilPrinting.js
@@ -34,6 +34,14 @@ function statement(invoice, plays) {
 		const result = Object.assign({}, aPerformance);
 		result.play = playFor(result);
 		result.amount = amountFor(result);
+		result.volumeCredits = volumeCreditsFor(result);
+		return result;
+	}
+
+	function volumeCreditsFor(aPerformance) {
+		let result = 0;
+		result += Math.max(aPerformance.audience - 30, 0);
+		if ('comedy' === aPerformance.play.type) result += Math.floor(perf.audience / 5);
 		return result;
 	}
 
@@ -81,17 +89,10 @@ function renderPlainText(data, plays) {
 		return result;
 	}
 
-	function volumeCreditsFor(aPerformance) {
-		let result = 0;
-		result += Math.max(aPerformance.audience - 30, 0);
-		if ('comedy' === aPerformance.play.type) result += Math.floor(perf.audience / 5);
-		return result;
-	}
-
 	function totalVolumeCredits() {
 		let result = 0;
 		for (let perf of data.performances) {
-			result += volumeCreditsFor(perf);
+			result += perf.volumeCredits;
 		}
 		return result;
 	}
