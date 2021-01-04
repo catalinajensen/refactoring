571bd104c373648031966dbf0ea724b3dadc0db1
Catalina Boznea <catalina.boznea@tradeshift.com>
2021-01-04 18:06:41 +0100

    Add createPerformanceCalculator

--- public/createStatementData.js
+++ public/createStatementData.js
@@ -7,7 +7,7 @@ export default function createStatementData(invoice, plays) {
 	return result;
 
 	function enrichPerformance(aPerformance) {
-		const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+		const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
 		const result = Object.assign({}, aPerformance);
 		result.play = calculator.play;
 		result.amount = calculator.amount;
@@ -69,4 +69,8 @@ class PerformanceCalculator {
 		if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
 		return result;
 	}
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+	return new PerformanceCalculator(aPerformance, aPlay);
 }
