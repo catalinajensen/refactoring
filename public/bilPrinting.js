cff85734ba312bac86732e86f594859229ed3e14
Catalina Boznea <catalina.boznea@tradeshift.com>
2021-01-04 15:56:29 +0100

    Add playFor function

--- public/bilPrinting.js
+++ public/bilPrinting.js
@@ -46,6 +46,10 @@ function amountFor(aPerformance, play) {
 	return result;
 }
 
+function playFor(aPerformance) {
+	return plays[aPerformance.playID];
+}
+
 function statement(invoice, plays) {
 	let totalAmount = 0;
 	let volumeCredits = 0;
@@ -57,7 +61,7 @@ function statement(invoice, plays) {
 	}).format;
 
 	for (let perf of invoice.performances) {
-		const play = plays[perf.playID];
+		const play = playFor(perf);
 		let thisAmount = amountFor(perf, play);
 
 		// add volume credits
