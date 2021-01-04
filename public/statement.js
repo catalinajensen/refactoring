572b318066179d3c7ba0ae3f5e0d21f26fb61ab7
Catalina Boznea <catalina.boznea@tradeshift.com>
2021-01-04 17:53:04 +0100

    Remove bilPrinting.js

--- public/statement.js
+++ public/statement.js
@@ -1,5 +1,43 @@
 import createStatementData from './createStatementData.js';
 
+const plays = {
+	hamlet: { name: 'Hamlet', type: 'tragedy' },
+	'as-like': { name: 'As You Like It', type: 'comedy' },
+	othello: { name: 'Othello', type: 'tragedy' }
+};
+
+const invoices = [
+	{
+		customer: 'BigCo',
+		performances: [
+			{
+				playID: 'hamlet',
+				audience: 55
+			},
+			{
+				playID: 'as-like',
+				audience: 35
+			},
+			{
+				playID: 'othello',
+				audience: 40
+			}
+		]
+	}
+];
+
+function statement(invoice, plays) {
+	return renderPlainText(createStatementData(invoice, plays));
+}
+function renderPlainText(data, plays) {
+	let result = `Statement for ${data.customer}\n`;
+	for (let perf of data.performances) {
+		result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+	}
+	result += `Amount owed is ${usd(data.totalAmount)}\n`;
+	result += `You earned ${data.totalVolumeCredits} credits\n`;
+	return result;
+}
 function htmlStatement(invoice, plays) {
 	return renderHtml(createStatementData(invoice, plays));
 }
@@ -16,7 +54,6 @@ function renderHtml(data) {
 	result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
 	return result;
 }
-
 function usd(aNumber) {
 	return new Intl.NumberFormat('en-US', {
 		style: 'currency',
@@ -24,3 +61,5 @@ function usd(aNumber) {
 		minimumFractionDigits: 2
 	}).format(aNumber / 100);
 }
+
+console.log(statement(invoices[0], plays));
